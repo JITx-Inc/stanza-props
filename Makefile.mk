d115fc7856d7ec8b7648625bcddb2fdaa3e664a5
jackbackrack <jrb@pobox.com>
2016-12-31 20:59:28 -0800

    new stanza

--- Makefile.mk
+++ Makefile.mk
@@ -3,6 +3,6 @@ stanza_props_all: ${GEN}/props.pkg
 ALL_PKG_DEPS += stanza_props_all
 
 ${GEN}/props.pkg: stanza-props/props.stanza ${GEN}/utils.pkg 
-	stanza $< $(STZ_FLAGS) -pkg ${GEN}
+	stanza $< $(STZ_FLAGS)
 
 
